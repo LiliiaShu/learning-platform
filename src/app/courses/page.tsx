f5a6789298af6ee80532da41eb3ba15a4aa9c095
Liliia <leyla92lilit@gmail.com>
2026-04-01 23:25:39 +0200

    fix: database schema push

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,5 +1,6 @@
 import { db } from "@/db";
 import { coursesTable } from "@/db/schema";
+export const dynamic = "force-dynamic";
 
 export default async function Courses() {
   const courses = await db.select().from(coursesTable);
